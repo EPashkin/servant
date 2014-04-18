0a93855d22619daff36c875956d7e5847f6398b6
Evgeny Pashkin <epashkin@cognitive.ru>
2014-04-18 21:58:09 +0400

    Added environment parameter for list of nodes allowed to connect

--- src/servant_app.erl
+++ src/servant_app.erl
@@ -13,23 +13,56 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
+    security_allow(),
     ?SUP:start_link().
 
 stop(_State) ->
     ok.
 
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
+
+security_allow() ->
+    CfgAllowed = application:get_env(?APP, 'allowed', []),
+    io:format("~p~n",[CfgAllowed]),
+    AllowedNodes = translate_config_allowed(CfgAllowed),
+    net_kernel:allow(AllowedNodes).
+
+translate_config_allowed([]) ->
+    [];
+translate_config_allowed([Name | Tail]) when is_list(Name) ->
+    NodeNameString = 
+        case lists:member($@, Name) of
+            true-> Name;
+            false -> CompName = net_adm:localhost(),
+                     Name ++ "@" ++ CompName
+        end,
+    NodeName = list_to_atom(NodeNameString),
+    [NodeName | translate_config_allowed(Tail)].
 
 %% ===================================================================
 %% Tests
 %% ===================================================================
 
--ifdef(TEST).
+%-ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
 
 start_stop_test() ->
+    ?assert(lists:member($@, "test@host")),
     ok = application:start(?APP),
     ?assertNot(undefined == whereis(?SUP)),
     ok = application:stop(?APP),
     ?assert(undefined == whereis(?SUP)).
 
--endif.
+translate_config_allowed_test_() ->
+    [?_assertEqual({Expected, Value}, {translate_config_allowed(Value), Value})
+     || {Expected, Value} <- 
+            [
+             {[], []},
+             {[test@host], ["test@host"]},
+             {[list_to_atom("test@"++net_adm:localhost())], ["test"]},
+             {[test@host, test1@host], ["test@host", "test1@host"]}
+            ]].
+
+%-endif.
